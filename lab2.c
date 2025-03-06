310e501a06592ee9667b52922d3c4d505129df50
Turcu Mihnea <mihnea2005@gmail.com>
2025-03-06 13:16:23 +0200

    impartire

--- lab2.c
+++ lab2.c
@@ -1,20 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int function(int x){
-	x=x-2;
-	x=function(x);
+int function(int x)
+{
+	x = x - 2;
 	return x;
 }
 
-int main(){
-	int a=30, i=0;
-	
-	while (a>0){
-		a=function(a);
+int main()
+{
+	int a = 30, i = 0;
+
+	while (a > 0)
+	{
+		a = function(a);
 		i++;
 	}
 	printf("%d", i);
-	return 0;	
+	return 0;
 }
-
